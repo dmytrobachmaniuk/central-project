7503ef26ff0f0bdef9616ce9c836a4b273e16911
Dmytro Bachmaniuk <dmytro2108@gmail.com>
2025-08-10 13:49:18 +0300

    chore: initialize project structure without data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Homepage from "@/pages/Homepage/Homepage.jsx";
+
+export default function App() {
+
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+  )
+}
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header/Header.jsx";
+import Footer from "@/components/Footer/Footer.jsx";
+
+const Homepage = () => {
+
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default Homepage
