99b8fe00f29ff61f91df2e0f4b80b737f09ddd63
Dmytro Bachmaniuk <dmytro2108@gmail.com>
2025-08-19 14:49:15 +0300

    added deploy

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/central-project/'  // <-- назва репозиторію на GitHub
+  base: "/central-project", // <-- назва репозиторію на GitHub
 })
